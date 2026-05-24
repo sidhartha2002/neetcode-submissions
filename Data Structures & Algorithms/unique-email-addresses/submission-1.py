a368b670668bd76f1678448b2e3f11a785cf05fb
Sidhartha Mohanty <mohantysidhartha2002@gmail.com>
2026-05-25 03:06:07 +0530

    Add: unique-email-addresses - submission-1

---
+++ Data Structures & Algorithms/unique-email-addresses/submission-1.py
@@ -0,0 +1,36 @@
+class Solution:
+    def numUniqueEmails(self, emails: List[str]) -> int:
+        # approach :
+        # 1. split the strings to local + domain part
+        # 2. check if local string conatins any '+' then consider the first part before '+' as per the '+' rule
+        # 3. check '.' present in the new local then replace it witn ""
+        # 4. Recombine the local + @ + domain to form unique email.
+        # 5. return the len of the set
+
+
+        unique_emails = set()
+
+        for email in emails:
+            # 1.
+            local, domain = email.split('@')
+
+            # 2.
+            if "+" in local:
+                local = local.split("+")[0] # split and consider only the first past before "+"
+            # 3.
+            local = local.replace(".", "")
+
+            # 4.
+            unique_emails.add(local + "@" + domain)
+            
+        # 5.
+        return len(unique_emails)
+
+
+
+
+
+
+
+
+            
